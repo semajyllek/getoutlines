c0c3cf6b274bcd40e1596a268dda92f2d832d551
James Kelly <jkelly@sensoryinc.com>
2024-11-23 22:03:24 -0800

    updated dino config

--- src/object_extractor/configs/groundingdino_config.py
+++ src/object_extractor/configs/groundingdino_config.py
@@ -30,15 +30,30 @@ max_position_embeddings = 256
 query_dim = 4
 num_feature_levels = 4
 nheads_fusion = 8
-transformer_activation = "relu"  # Added
-dec_n_points = 4  # Added
-enc_n_points = 4  # Added
-two_stage_num_proposals = 900  # Added
-assign_first_stage = True  # Added
-with_mask = True  # Added
-mask_dim = 256  # Added
-freeze_text_encoder = True  # Added
-random_refpoints_xy = False  # Added
+transformer_activation = "relu"
+dec_n_points = 4
+enc_n_points = 4
+
+# Two-Stage Parameters
+two_stage_type = 'standard'  # Added
+two_stage_pat_embed = 0  # Added
+two_stage_add_query_num = 0  # Added
+two_stage_bbox_embed_share = False  # Added
+two_stage_class_embed_share = False  # Added
+two_stage_learn_wh = False  # Added
+two_stage_default_hw = 0.1  # Added
+two_stage_keep_all_tokens = False  # Added
+two_stage_num_proposals = 900
+assign_first_stage = True
+embed_init_tgt = True  # Added
+use_text_enhancer = False  # Added
+
+# Additional Model Parameters
+with_mask = True
+mask_dim = 256
+freeze_text_encoder = True
+random_refpoints_xy = False
+dn_labelbook_size = 100  # Added
 
 # Positional Encoding
 pe_temperatureH = 20
