15665fd40573f3395451661a9ba3a0218a82b7de
James Kelly <jkelly@sensoryinc.com>
2024-11-24 07:39:21 -0800

    updated dino config, greatly simplified lol

--- src/object_extractor/configs/groundingdino_config.py
+++ src/object_extractor/configs/groundingdino_config.py
@@ -10,17 +10,9 @@ text_encoder_type = "bert-base-uncased"
 enc_layers = 6
 dec_layers = 6
 pre_norm = False
-hidden_dim = 256
+normalize_before = False
 
-# Backbone Config
-backbone = 'swin_T_224_1k'
-return_interm_indices = [1, 2, 3]
-
-# Detection Parameters
-text_threshold = 0.25
-box_threshold = 0.35
-
-# These are required by the model architecture but we don't modify them
+# Transformer Parameters
 nheads = 8
 dim_feedforward = 2048
 dropout = 0.0
@@ -29,4 +21,40 @@ dec_n_points = 4
 transformer_activation = "relu"
 num_patterns = 0
 num_feature_levels = 4
+
+# Position Encoding
+pe_temperatureH = 20
+pe_temperatureW = 20
+pe_temperature = 20
+query_dim = 4
+random_refpoints_xy = False
+
+# Backbone Config
+backbone = 'swin_T_224_1k'
+return_interm_indices = [1, 2, 3]
+backbone_freeze_keywords = None
 use_checkpoint = True
+dilation = False
+
+# Detection Settings
+text_threshold = 0.25
+box_threshold = 0.35
+with_box_refine = True
+two_stage = True
+two_stage_type = 'standard'
+two_stage_num_proposals = 900
+assign_first_stage = True
+embed_init_tgt = True
+use_text_enhancer = False
+dec_pred_bbox_embed_share = True
+dec_pred_class_embed_share = True
+use_text_cross_attention = True
+text_dropout = 0.1
+use_fusion_layer = True
+use_checkpoint_for_fusion = True
+use_transformer_ckpt = True
+use_checkpoint_for_encoder = True
+use_checkpoint_for_decoder = True
+sub_sentence_present = True
+enforce_input_proj = False
+return_intermediate_dec = True
